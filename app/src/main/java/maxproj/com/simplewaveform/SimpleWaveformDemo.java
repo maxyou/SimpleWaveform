b6dc9d789f55f264e6a21667c9738485b6b9e8f8
Hongyu You <hyyou@foxmail.com>
2016-01-01 18:36:17 +0800

    first commit

---
+++ app/src/main/java/maxproj/com/simplewaveform/SimpleWaveformDemo.java
@@ -0,0 +1,13 @@
+package maxproj.com.simplewaveform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleWaveformDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_waveform_demo);
+    }
+}
