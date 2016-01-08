7116ba2202103202445fa988830aa4745fdb61d6
Hongyu You <hyyou@foxmail.com>
2016-01-09 01:56:21 +0800

    advance demo 1, for sound recorder

--- app/src/main/java/maxproj/com/simplewaveform/SimpleWaveformDemo.java
+++ app/src/main/java/maxproj/com/simplewaveform/SimpleWaveformDemo.java
@@ -16,10 +16,11 @@ import java.util.Random;
 
 public class SimpleWaveformDemo extends AppCompatActivity {
 
-    Button button_switch;
-    Button button_goto_advance;
+    Button button_switch_simple_demos;
+    Button button_switch_advance_demos;
 
-    int demo_loop;
+    int simple_demo_loop = 0;
+    int advance_demo_loop = 0;
 
     SimpleWaveform simpleWaveform;
 
@@ -35,29 +36,53 @@ public class SimpleWaveformDemo extends AppCompatActivity {
 
         simpleWaveform = (SimpleWaveform)findViewById(R.id.simplewaveform);
 
-        button_switch = (Button)findViewById(R.id.button_switch);
-        button_switch.setOnClickListener(new View.OnClickListener() {
+        button_switch_simple_demos = (Button)findViewById(R.id.button_switch_simple_demos);
+        button_switch_simple_demos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                demo_loop++;
-                switch (demo_loop % 4){
-                    case 0:
+                advance_demo_loop = 0;
+                simple_demo_loop++;
+                if(simple_demo_loop > 4){
+                    simple_demo_loop = 1;
+                }
+                switch (simple_demo_loop) {
+                    case 1:
                         demo1();
                         break;
-                    case 1:
+                    case 2:
                         demo2();
                         break;
-                    case 2:
+                    case 3:
                         demo3();
                         break;
-                    case 3:
+                    case 4:
                         demo4();
                         break;
                 }
             }
         });
-        button_goto_advance = (Button)findViewById(R.id.button_goto_advance);
+        button_switch_advance_demos = (Button)findViewById(R.id.button_switch_advance_demos);
+        button_switch_advance_demos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simple_demo_loop = 0;
+                advance_demo_loop++;
+                if(advance_demo_loop > 2){
+                    advance_demo_loop = 1;
+                }
+                switch (advance_demo_loop) {
+                    case 1:
+                        demoAdvance1();
+                        break;
+                    case 2:
+                        demo2();
+                        break;
+                }
 
+            }
+        });
+
+        simple_demo_loop = 1;
         demo1();
 
 
@@ -341,6 +366,100 @@ public class SimpleWaveformDemo extends AppCompatActivity {
         };
         //show...
         simpleWaveform.refresh();
+    }
+
+
+
+    private void demoAdvance1() {
+
+        simpleWaveform.init();
+
+        final LinkedList<Integer> ampList = new LinkedList<>();
+        simpleWaveform.setDataList(ampList);
+
+        //define bar gap
+        simpleWaveform.barGap = 30;
+
+        //define x-axis direction
+        simpleWaveform.modeDirection = SimpleWaveform.MODE_DIRECTION_LEFT_RIGHT;
+
+        //define if draw opposite pole when show bars
+        simpleWaveform.modeAmp = SimpleWaveform.MODE_AMP_ABSOLUTE;
+        //define if the unit is px or percent of the view's height
+        simpleWaveform.modeHeight = SimpleWaveform.MODE_HEIGHT_PERCENT;
+        //define where is the x-axis in y-axis
+        simpleWaveform.modeZero = SimpleWaveform.MODE_ZERO_CENTER;
+        //if show bars?
+        simpleWaveform.showBar = true;
+
+        //define how to show peaks outline
+        simpleWaveform.modePeak = SimpleWaveform.MODE_PEAK_PARALLEL;
+        //if show peaks outline?
+        simpleWaveform.showPeak = true;
+
+        //define pencil to draw bar
+        barPencilFirst.setStrokeWidth(15);
+        barPencilFirst.setColor(0xff1dcf0f);
+        simpleWaveform.barPencilFirst = barPencilFirst;
+        barPencilSecond.setStrokeWidth(15);
+        barPencilSecond.setColor(0xff1dcfcf);
+        simpleWaveform.barPencilSecond = barPencilSecond;
+
+        //define pencil to draw peaks outline
+        peakPencilFirst.setStrokeWidth(5);
+        peakPencilFirst.setColor(0xfffe2f3f);
+        simpleWaveform.peakPencilFirst = peakPencilFirst;
+        peakPencilSecond.setStrokeWidth(5);
+        peakPencilSecond.setColor(0xfffeef3f);
+        simpleWaveform.peakPencilSecond = peakPencilSecond;
+
+        //the first part will be draw by PencilFirst
+        simpleWaveform.firstPartNum = 20;
+
+        //define how to clear screen
+        simpleWaveform.clearScreenListener = new SimpleWaveform.ClearScreenListener() {
+            @Override
+            public void clearScreen(Canvas canvas) {
+                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            }
+        };
+        simpleWaveform.progressTouch = new SimpleWaveform.ProgressTouch() {
+            @Override
+            public void progressTouch(int progress, MotionEvent event) {
+                Log.d("", "you touch at: " + progress);
+                simpleWaveform.firstPartNum = progress;
+                simpleWaveform.refresh();
+            }
+        };
+        //loop
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                    }
+
+                    if(advance_demo_loop != 1){
+                        break;
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ampList.addFirst(randomInt(-50, 50));
+                            if(ampList.size() > simpleWaveform.width / simpleWaveform.barGap + 2){
+                                ampList.removeLast();
+                                Log.d("","SimpleWaveform: ampList remove last node, total " + ampList.size());
+                            }
+                            simpleWaveform.refresh();
+                        }
+                    });
+                }
+            }
+        }).start();
+
     }
 
     public int randomInt(int min, int max) {
